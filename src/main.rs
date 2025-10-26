8f6c699ddad65c636a18ce254c2951e90c8b1b18
Hiruto1337 <https://Hiruto1337@github.com>
2025-10-26 13:07:05 +0100

    Improve code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 use std::{
-    io::{BufRead, BufReader, Write},
+    io::{stdout, BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
     sync::{Arc, RwLock},
-    thread,
+    thread, time::Duration,
 };
+
+struct Message {
+    sender: String,
+    msg: String,
+    from_self: bool,
+}
+
+impl std::fmt::Display for Message {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Message {
+            sender,
+            msg,
+            from_self,
+        } = self;
+
+        let color = if *from_self {
+            "\x1b[1;32m" // Set color to green
+        } else {
+            "\x1b[1;31m" // Set color to red
+        };
+
+        write!(f, "{color}<{sender}>\x1b[0m{msg}")
+    }
+}
 
 fn start_server_tunnel(addr: String) {
     let clients: Arc<RwLock<Vec<(String, Arc<TcpStream>)>>> = Arc::new(RwLock::new(vec![]));
@@ -59,11 +83,21 @@ fn start_server_tunnel(addr: String) {
 }
 
 fn start_client(name: String, addr: String) {
+    let neutralize_cursor = || {
+        print!("\x1b[43;1H");
+        print!("Message: ");
+        stdout().flush().unwrap();
+    };
+
+    neutralize_cursor();
+
+    let mut messages: Vec<Message> = vec![];
     // Create thread that prints incoming lines
     let stream = Arc::new(TcpStream::connect(addr).unwrap());
     let stream_read = stream.clone();
     let stream_write = stream.clone();
     let name_clone = name.clone();
+    let mut message_position = 1;
 
     thread::spawn(move || {
         let reader = BufReader::new(stream_read.as_ref());
@@ -71,15 +105,27 @@ fn start_client(name: String, addr: String) {
         for line in reader.lines() {
             match line {
                 Ok(msg) => {
+                    // Save incoming message
                     let (sender, msg) = msg.split_once(':').unwrap();
 
-                    if sender == name_clone {
-                        print!("\x1b[1;32m");
-                    } else {
-                        print!("\x1b[1;31m");
+                    let message = Message {
+                        sender: sender.to_string(),
+                        msg: msg.to_string(),
+                        from_self: sender == name_clone.as_str(),
                     };
-                    print!("{sender}");
-                    println!("\x1b[0m{msg}");
+
+                    messages.push(message);
+
+                    // Save current cursor position
+                    print!("\x1b[s");
+                    // Move cursor up to line position
+                    print!("\x1b[{message_position};1H");
+                    // Print the message
+                    print!(" {}", messages.last().unwrap());
+                    // Return to saved cursor position
+                    print!("\x1b[u");
+                    stdout().flush().unwrap();
+                    message_position += 1;
                 }
                 Err(_) => println!("Error!"),
             }
@@ -92,10 +138,12 @@ fn start_client(name: String, addr: String) {
     for line in stdin.lock().lines() {
         match line {
             Ok(msg) => {
-                print!("\x1b[1A"); // Move cursor up by 1
-                print!("\x1b[1K"); // Delete from beginning of line to position
-                print!("\x1b[1G"); // Place cursor at column 1
-                let _ = writeln!(stream_write.as_ref(), "{name}: {msg}");
+                neutralize_cursor();
+                print!("\x1b[0J"); // Delete from cursor to end of screen
+                std::io::stdout().flush().unwrap();
+                if msg.len() != 0 {
+                    let _ = writeln!(stream_write.as_ref(), "{name}: {msg}");
+                }
             }
             Err(_) => println!("Error!"),
         }
@@ -103,9 +151,10 @@ fn start_client(name: String, addr: String) {
 }
 
 fn main() {
-    print!("\x1b[2J"); // Clear terminal
-    print!("\x1b[H"); // Move cursor to start
-    std::io::stdout().flush().unwrap();
+    // Clear terminal
+    std::process::Command::new("clear").spawn().unwrap();
+    std::thread::sleep(Duration::from_millis(50));
+
     let args: Vec<String> = std::env::args().collect();
 
     match (
