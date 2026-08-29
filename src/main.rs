23e6f039898c1cd9811bbf7644863a5397bb65db
Lasse Grosbøl-Rais <latios7777777@gmail.com>
2026-08-29 17:58:29 +0200

    Remove command line arguments + Refactor

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ pub mod misc;
 
 use std::{
     io::stdout,
-    sync::{Arc, Mutex}, thread::sleep, time::Duration,
+    sync::{Arc, Mutex, atomic::{AtomicBool, Ordering::Relaxed}}, thread::{JoinHandle, sleep}, time::Duration,
 };
 
 use anyhow::{Context, Result};
@@ -17,24 +17,67 @@ const ALPN: &[u8] = b"crypchat";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut args = std::env::args().skip(1);
+    // Get display name
+    let name = get_input("Display name: ");
+    
+    // Go online and display peer ID
+    execute!(stdout(), MoveToColumn(0), Print("My peer ID: "), Hide).unwrap();
 
-    match (args.next(), args.next()) {
-        (Some(name), None) => start_client(name, None).await?,
-        (Some(name), Some(peer_id)) => {
-            let peer_endpoint: EndpointId = peer_id.parse()?;
-            start_client(name, Some(peer_endpoint)).await?
-        },
-        _ => {
-            println!("Error: Must provide arguments \"[name] [peer_id?]\"");
-            return Ok(());
+    //// Connect to the p2p network
+    let ep: Endpoint = Endpoint::builder(presets::N0)
+        .alpns(vec![ALPN.to_vec()])
+        .bind()
+        .await?;
+
+    //// Wait until user is connected
+    let (animation, kill) = loading_animation();
+    ep.online().await;
+
+    //// Kill animation
+    kill.store(true, Relaxed);
+    animation.join().unwrap();
+
+    //// Show peer ID
+    execute!(stdout(), Print(ep.id().to_string() + "\n")).unwrap();
+
+    // Get other peer ID
+    let peer_id: EndpointId = get_input("Other peer ID: ").parse().unwrap();
+
+    // Establish connection
+    execute!(stdout(), Print("Connecting")).unwrap();
+
+    let (animation, kill) = loading_animation();
+
+    // Decide caller and receiver based on peer ID
+    let smallest_id = ep.id().to_string() < peer_id.to_string();
+    let (conn, mut write_stream, read_stream);
+    
+    loop {
+        let conn_ = if smallest_id {
+            ep.accept().await.context("Accept failed")?.await?
+        } else {
+            ep.connect(peer_id, ALPN).await?
+        };
+
+        let (write_stream_, read_stream_) = if smallest_id {
+            conn_.accept_bi().await?
+        } else {
+            conn_.open_bi().await?
+        };
+
+        // Only accept peer with appropriate ID
+        if conn_.remote_id().to_string() == peer_id.to_string() {
+            conn = conn_;
+            write_stream = write_stream_;
+            read_stream = read_stream_;
+            break;
         }
     }
 
-    Ok(())
-}
+    // Kill loading animation
+    kill.store(true, Relaxed);
+    animation.join().unwrap();
 
-async fn start_client(name: String, peer_id: Option<EndpointId>) -> Result<()> {
     // Enter raw mode and take full control of scrolling behavior
     crossterm::terminal::enable_raw_mode().unwrap();
     execute!(
@@ -46,50 +89,9 @@ async fn start_client(name: String, peer_id: Option<EndpointId>) -> Result<()> {
     // Create the terminal representative
     let terminal = Arc::new(Mutex::new(Terminal::from(name)));
 
-    // Draw initial UI⠤
-    terminal.lock().unwrap().draw();
-
-    execute!(stdout(), MoveToColumn(0), Print("Connection: "), Hide).unwrap();
-
-    // Connect to the p2p network
-    let ep: Endpoint = Endpoint::builder(presets::N0)
-        .alpns(vec![ALPN.to_vec()])
-        .bind()
-        .await?;
-
-    // Wait until user is connected
-    {
-        let animation_read = Arc::new(Mutex::new(true));
-        let animation_write = animation_read.clone();
-
-        let loading_animation = std::thread::spawn(move || {
-            let mut animation = "⠋⠙⠸⢰⣠⣄⡆⠇".chars().cycle();
-
-            while *animation_read.lock().unwrap() {
-                execute!(stdout(), Print(animation.next().unwrap()), MoveLeft(1)).unwrap();
-                sleep(Duration::from_millis(125));
-            }
-        });
-
-        ep.online().await;
-        *animation_write.lock().unwrap() = false;
-        loading_animation.join().unwrap();
-    }
-
-    execute!(stdout(), Print(ep.id().to_string())).unwrap();
-
-    let (conn, mut write_stream, read_stream) = if let Some(peer_id) = peer_id {
-        let conn = ep.connect(peer_id, ALPN).await?;
-        let (write_stream, read_stream) = conn.open_bi().await?;
-        (conn, write_stream, read_stream)
-    } else {
-        let conn = ep.accept().await.context("Accept failed")?.await?;
-        let (write_stream, read_stream) = conn.accept_bi().await?;
-        (conn, write_stream, read_stream)
-    };
-
     execute!(stdout(), Show, EnableBlinking).unwrap();
 
+    // Draw initial UI
     terminal.lock().unwrap().draw();
 
     let (tx, mut rx) = tokio::sync::mpsc::channel::<String>(1024);
@@ -117,8 +119,6 @@ async fn start_client(name: String, peer_id: Option<EndpointId>) -> Result<()> {
             write_stream.write_all(message.as_bytes()).await.unwrap();
         }
     });
-
-    // TODO: Only accept peer with appropriate ID
 
     // Create thread that reacts to incoming data
     let terminal_clone = terminal.clone();
@@ -167,4 +167,29 @@ async fn start_client(name: String, peer_id: Option<EndpointId>) -> Result<()> {
             _ => {}
         }
     }
+}
+
+fn get_input(prompt: &str) -> String {
+    execute!(stdout(), Print(format!("{}", prompt))).unwrap();
+
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).unwrap();
+    buffer.pop().unwrap();
+    buffer
+}
+
+fn loading_animation() -> (JoinHandle<()>, Arc<AtomicBool>){
+    let kill = Arc::new(AtomicBool::new(false));
+    let killer = kill.clone();
+
+    let animation = std::thread::spawn(move || {
+        let mut animation = "⠋⠙⠸⢰⣠⣄⡆⠇".chars().cycle();
+
+        while kill.load(Relaxed) == false {
+            execute!(stdout(), Print(animation.next().unwrap()), MoveLeft(1)).unwrap();
+            sleep(Duration::from_millis(125));
+        }
+    });
+
+    (animation, killer)
 }
