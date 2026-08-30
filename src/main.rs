e6b53c18ac6cc9314e2311b4e487f1fcea4f8a81
Lasse Grosbøl-Rais <latios7777777@gmail.com>
2026-08-30 09:29:01 +0200

    Add animation macro + Move thread spawning to functions

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,40 @@ pub mod misc;
 
 use std::{
     io::stdout,
-    sync::{Arc, Mutex, atomic::{AtomicBool, Ordering::Relaxed}}, thread::{JoinHandle, sleep}, time::Duration,
+    sync::{Arc, Mutex, atomic::{AtomicBool, Ordering::Relaxed}}, thread::sleep, time::Duration,
 };
 
 use anyhow::{Context, Result};
-use crossterm::{cursor::{EnableBlinking, Hide, MoveLeft, MoveToColumn, Show}, event::Event, execute, style::Print};
-use iroh::{Endpoint, EndpointId, endpoint::presets};
-use tokio::io::{AsyncBufReadExt, BufReader};
+use crossterm::{cursor::{EnableBlinking, Hide, MoveLeft, Show}, event::Event, execute, style::Print};
+use iroh::{Endpoint, EndpointId, endpoint::{Connection, RecvStream, SendStream, presets}};
+use tokio::{io::{AsyncBufReadExt, BufReader}, sync::mpsc::{Receiver, Sender}};
 
 use crate::misc::terminal::Terminal;
+
+macro_rules! load {
+    ($($code:tt)*) => {
+        let kill = Arc::new(AtomicBool::new(false));
+        let killer = kill.clone();
+
+        // Spawn animation thread
+        let animation = std::thread::spawn(move || {
+            let mut animation = "⠋⠙⠸⢰⣠⣄⡆⠇".chars().cycle();
+
+            while kill.load(Relaxed) == false {
+                execute!(stdout(), Print(animation.next().unwrap()), MoveLeft(1)).unwrap();
+                sleep(Duration::from_millis(125));
+            }
+        });
+
+        // Code
+        $($code)*
+
+        // Kill animation thread
+        killer.store(true, Relaxed);
+        animation.join().unwrap();
+        drop(killer);
+    };
+}
 
 const ALPN: &[u8] = b"crypchat";
 
@@ -21,61 +46,51 @@ async fn main() -> Result<()> {
     let name = get_input("Display name: ");
     
     // Go online and display peer ID
-    execute!(stdout(), MoveToColumn(0), Print("My peer ID: "), Hide).unwrap();
+    execute!(stdout(), Print("My peer ID: "), Hide).unwrap();
 
-    //// Connect to the p2p network
-    let ep: Endpoint = Endpoint::builder(presets::N0)
-        .alpns(vec![ALPN.to_vec()])
-        .bind()
-        .await?;
+    load!{
+        let ep: Endpoint = Endpoint::builder(presets::N0)
+            .alpns(vec![ALPN.to_vec()])
+            .bind()
+            .await?;
 
-    //// Wait until user is connected
-    let (animation, kill) = loading_animation();
-    ep.online().await;
+        ep.online().await;
+    };
 
-    //// Kill animation
-    kill.store(true, Relaxed);
-    animation.join().unwrap();
-
-    //// Show peer ID
     execute!(stdout(), Print(ep.id().to_string() + "\n")).unwrap();
 
     // Get other peer ID
     let peer_id: EndpointId = get_input("Other peer ID: ").parse().unwrap();
 
     // Establish connection
-    execute!(stdout(), Print("Connecting")).unwrap();
+    execute!(stdout(), Print("Connecting ")).unwrap();
 
-    let (animation, kill) = loading_animation();
+    load!{
+        // Decide caller and receiver based on peer ID
+        let smallest_id = ep.id().to_string() < peer_id.to_string();
 
-    // Decide caller and receiver based on peer ID
-    let smallest_id = ep.id().to_string() < peer_id.to_string();
+        let (conn, write_stream, read_stream) = loop {
+            let conn = if smallest_id {
+                ep.accept().await.context("Accept failed")?.await?
+            } else {
+                ep.connect(peer_id, ALPN).await?
+            };
 
-    let (conn, mut write_stream, read_stream) = loop {
-        let conn_ = if smallest_id {
-            ep.accept().await.context("Accept failed")?.await?
-        } else {
-            ep.connect(peer_id, ALPN).await?
+            // If the connection isn't from the expected peer, abort
+            if conn.remote_id() != peer_id {
+                conn.close(1u8.into(), b"Unauthorized");
+                continue;
+            }
+
+            let (write_stream, read_stream) = if smallest_id {
+                conn.accept_bi().await?
+            } else {
+                conn.open_bi().await?
+            };
+
+            break (conn, write_stream, read_stream);
         };
-
-        // If the connection isn't from the expected peer, abort
-        if conn_.remote_id() != peer_id {
-            conn_.close(1u8.into(), b"Unauthorized");
-            continue;
-        }
-
-        let (write_stream_, read_stream_) = if smallest_id {
-            conn_.accept_bi().await?
-        } else {
-            conn_.open_bi().await?
-        };
-
-        break (conn_, write_stream_, read_stream_);
     };
-
-    // Kill loading animation
-    kill.store(true, Relaxed);
-    animation.join().unwrap();
 
     // Enter raw mode and take full control of scrolling behavior
     crossterm::terminal::enable_raw_mode().unwrap();
@@ -93,63 +108,16 @@ async fn main() -> Result<()> {
     // Draw initial UI
     terminal.lock().unwrap().draw();
 
-    let (tx, mut rx) = tokio::sync::mpsc::channel::<String>(1024);
+    let (tx, rx) = tokio::sync::mpsc::channel::<String>(1024);
     terminal.lock().unwrap().tx = Some(tx.clone());
 
     // Use a single centralized thread to send messages
-    tokio::spawn(async move {
-        while let Some(message) = rx.recv().await {
-            if message == "/status" {
-                for path in &conn.paths() {
-                    if path.is_selected() == false {
-                        continue;
-                    }
-
-                    if path.is_ip() {
-                        execute!(stdout(), Print("👤⇄👤")).unwrap();
-                        break;
-                    } else if path.is_relay() {
-                        execute!(stdout(), Print("👤⇄📻⇄👤")).unwrap();
-                        break;
-                    }
-                }
-                continue;
-            }
-            write_stream.write_all(message.as_bytes()).await.unwrap();
-        }
-    });
+    spawn_writer(conn, write_stream, rx);
 
     // Create thread that reacts to incoming data
     let terminal_clone = terminal.clone();
     let tx_clone = tx.clone();
-
-    tokio::spawn(async move {
-        let reader = BufReader::new(read_stream);
-        let mut lines = reader.lines();
-
-        while let Ok(Some(line)) = lines.next_line().await {
-            let response = {
-                let mut lock = terminal_clone.lock().unwrap();
-
-                if lock.cipher.is_some() {
-                    // Save message
-                    lock.save_message(line);
-                    None
-                } else if line != lock.ec_point.to_string() {
-                    // If incoming EC point is not my own
-                    // Create cipher and reciprocate my own EC point
-                    lock.create_cipher(line);
-                    Some(lock.ec_point.to_string() + "\n")
-                } else {
-                    None
-                }
-            };
-
-            if let Some(ec_point) = response {
-                tx_clone.send(ec_point).await.unwrap();
-            }
-        }
-    });
+    spawn_reader(read_stream, terminal_clone, tx_clone);
 
     // Announce elliptic curve point to peer
     let ec_point = terminal.lock().unwrap().ec_point.to_string() + "\n";
@@ -177,18 +145,56 @@ fn get_input(prompt: &str) -> String {
     buffer
 }
 
-fn loading_animation() -> (JoinHandle<()>, Arc<AtomicBool>){
-    let kill = Arc::new(AtomicBool::new(false));
-    let killer = kill.clone();
+fn spawn_writer(conn: Connection, mut write_stream: SendStream, mut rx: Receiver<String>) {
+    tokio::spawn(async move {
+        while let Some(message) = rx.recv().await {
+            if message == "/status" {
+                for path in &conn.paths() {
+                    if path.is_selected() == false {
+                        continue;
+                    }
 
-    let animation = std::thread::spawn(move || {
-        let mut animation = "⠋⠙⠸⢰⣠⣄⡆⠇".chars().cycle();
-
-        while kill.load(Relaxed) == false {
-            execute!(stdout(), Print(animation.next().unwrap()), MoveLeft(1)).unwrap();
-            sleep(Duration::from_millis(125));
+                    if path.is_ip() {
+                        execute!(stdout(), Print("👤⇄👤")).unwrap();
+                        break;
+                    } else if path.is_relay() {
+                        execute!(stdout(), Print("👤⇄📻⇄👤")).unwrap();
+                        break;
+                    }
+                }
+                continue;
+            }
+            write_stream.write_all(message.as_bytes()).await.unwrap();
         }
     });
+}
 
-    (animation, killer)
+fn spawn_reader(read_stream: RecvStream, terminal: Arc<Mutex<Terminal>>, tx: Sender<String>) {
+    tokio::spawn(async move {
+        let reader = BufReader::new(read_stream);
+        let mut lines = reader.lines();
+
+        while let Ok(Some(line)) = lines.next_line().await {
+            let response = {
+                let mut lock = terminal.lock().unwrap();
+
+                if lock.cipher.is_some() {
+                    // Save message
+                    lock.save_message(line);
+                    None
+                } else if line != lock.ec_point.to_string() {
+                    // If incoming EC point is not my own
+                    // Create cipher and reciprocate my own EC point
+                    lock.create_cipher(line);
+                    Some(lock.ec_point.to_string() + "\n")
+                } else {
+                    None
+                }
+            };
+
+            if let Some(ec_point) = response {
+                tx.send(ec_point).await.unwrap();
+            }
+        }
+    });
 }
