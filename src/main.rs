b2fc58d7ec7facb35cd466599c009d585ce7c6a4
Lasse Grosbøl-Rais <latios7777777@gmail.com>
2026-09-10 09:28:34 +0200

    Refactor code (move main logic to functions)

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use std::{
 };
 
 use anyhow::{Context, Result};
-use crossterm::{cursor::EnableBlinking, event::Event, execute, style::Print};
-use iroh::{Endpoint, EndpointId, endpoint::presets};
+use crossterm::{cursor::EnableBlinking, event::Event, execute, style::Print, terminal::EnterAlternateScreen};
+use iroh::{Endpoint, EndpointId, endpoint::{Connection, RecvStream, SendStream, presets}};
 use tokio::sync::mpsc::channel;
 
 use crate::misc::{terminal::Terminal, threads::{spawn_reader, spawn_writer, LoadingAnimation}};
@@ -32,14 +32,7 @@ fn get_input(prompt: &str) -> String {
     buffer
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    // Get display name
-    let name = get_input("Display name: ");
-    
-    // Go online and display peer ID
-    execute!(stdout(), Print("My peer ID: ")).unwrap();
-
+async fn join_network() -> Result<Endpoint> {
     load!{
         let ep: Endpoint = Endpoint::builder(presets::N0)
             .alpns(vec![ALPN.to_vec()])
@@ -49,14 +42,10 @@ async fn main() -> Result<()> {
         ep.online().await;
     };
 
-    execute!(stdout(), Print(ep.id().to_string() + "\n")).unwrap();
+    Ok(ep)
+}
 
-    // Get other peer ID
-    let peer_id: EndpointId = get_input("Other peer ID: ").parse().unwrap();
-
-    // Establish connection
-    execute!(stdout(), Print("Connecting ")).unwrap();
-
+async fn connect_to_peer(ep: &Endpoint, peer_id: EndpointId) -> Result<(Connection, SendStream, RecvStream)> {
     load!{
         // Decide caller and receiver based on peer ID
         let smallest_id = ep.id().to_string() < peer_id.to_string();
@@ -68,30 +57,44 @@ async fn main() -> Result<()> {
                 ep.connect(peer_id, ALPN).await?
             };
 
-            // If the connection isn't from the expected peer, abort
-            if conn.remote_id() != peer_id {
-                conn.close(1u8.into(), b"Unauthorized");
-                continue;
+            if conn.remote_id() == peer_id {
+                let (write_stream, read_stream) = if smallest_id {
+                    conn.accept_bi().await?
+                } else {
+                    conn.open_bi().await?
+                };
+
+                break (conn, write_stream, read_stream);
             }
 
-            let (write_stream, read_stream) = if smallest_id {
-                conn.accept_bi().await?
-            } else {
-                conn.open_bi().await?
-            };
-
-            break (conn, write_stream, read_stream);
+            // If the connection isn't from the expected peer, close connection and retry
+            conn.close(1u8.into(), b"Unauthorized");
         };
     };
 
+    Ok((conn, write_stream, read_stream))
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Get display name
+    let name = get_input("Display name: ");
+    
+    // Go online and display peer ID
+    execute!(stdout(), Print("My peer ID: ")).unwrap();
+    let ep = join_network().await?;
+    execute!(stdout(), Print(ep.id().to_string() + "\n")).unwrap();
+
+    // Get other peer ID
+    let peer_id: EndpointId = get_input("Other peer ID: ").parse().unwrap();
+
+    // Establish connection
+    execute!(stdout(), Print("Connecting ")).unwrap();
+    let (conn, write_stream, read_stream) = connect_to_peer(&ep, peer_id).await?;
+
     // Enter raw mode and take full control of scrolling behavior
     crossterm::terminal::enable_raw_mode().unwrap();
-    execute!(
-        stdout(),
-        crossterm::terminal::EnterAlternateScreen,
-        EnableBlinking
-    )
-    .unwrap();
+    execute!(stdout(), EnterAlternateScreen, EnableBlinking).unwrap();
 
     // Create the terminal representative
     let (tx, rx) = channel::<String>(1024);
